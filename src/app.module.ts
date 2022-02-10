da4f9f994c96e8f5ead0577ca5da8c3f68c675c5
Adan Figueroa <adan.figueroa@appliedcognition.com>
2022-02-09 19:47:04 -0600

    config file, schema and interface

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
+import config from './config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [GameModule],
+  imports: [MongooseModule.forRoot(config.DB_URL), GameModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export default {
+  PORT: parseInt(process.env.PORT, 10) || 3000,
+  DB_URL: process.env.DB_URL,
+};
